649a70320218c0e85526ae2a9f1de43a25058fba
Liza Dudyk <55206822+lizooo@users.noreply.github.com>
2020-11-26 22:18:27 +0200

    Added models

---
+++ src/models/movies.model.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var mysqlConnection = require('./../../config/db.config');
+console.log(mysqlConnection)
+
+//Movie object create
+var Movie = function(movie){
+  this.name           = movie.Name;
+  this.durationMins   = movie.DurationMins;
+  this.imdbReviews    = movie.IMDBReviews;
+};
+
+Movie.create = function (newMovie, result) {
+mysqlConnection.query("INSERT INTO movie set ?", newMovie, function (err, res) {
+if(err) {
+  console.log("error: ", err);
+  result(err, null);
+}
+else{
+  console.log(res.insertId);
+  result(null, res.insertId);
+}
+});
+};
+
+Movie.findById = function (MovieID, result) {
+mysqlConnection.query("Select * from movie where MovieID = ? ", MovieID, function (err, res) {
+if(err) {
+  console.log("error: ", err);
+  result(err, null);
+}
+else{
+  result(null, res);
+}
+});
+};
+
+Movie.findAll = function (result) {
+  mysqlConnection.query("Select * from movie", function (err, res) {
+  if(err) {
+    console.log("error: ", err);
+    result(null, err);
+  }
+  else{
+    console.log('movies : ', res);
+    result(null, res);
+  }
+  });
+};
+
+Movie.update = function(MovieID, movie, result){
+  mysqlConnection.query("UPDATE movie SET Name = ?,  DurationMins = ?, IMDBReviews = ? WHERE MovieID = ?", [movie.name, movie.durationMins, movie.imdbReviews, MovieID], function (err, res) {
+  if(err) {
+    console.log("error: ", err);
+    result(null, err);
+  }else{
+    result(null, res);
+  }
+});
+};
+
+Movie.delete = function(MovieID, result){
+mysqlConnection.query("DELETE FROM movie WHERE MovieID = ?", [MovieID], function (err, res) {
+if(err) {
+  console.log("error: ", err);
+  result(null, err);
+}
+else{
+  result(null, res);
+}
+});
+};
+
+module.exports= Movie;
